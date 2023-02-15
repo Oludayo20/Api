c9502c1b5319603036b3f411e49e8d4cb04c42fc
Oludayo <akinpelu.dayo11@gmail.com>
2023-02-15 02:42:17 +0100

    fds

--- routes/authRoutes.js
+++ routes/authRoutes.js
@@ -3,5 +3,7 @@ const router = express.Router();
 const authController = require('../controllers/authController');
 
 router.route('/').post(authController.login);
+router.route('/refresh').get(authController.refresh);
+router.route('/logout').post(authController.logout);
 
 module.exports = router;
